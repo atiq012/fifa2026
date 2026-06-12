ab053090eadfcdb46419c9d01e76427fe84e936e
atiq012 <shakilratiq@gmail.com>
2026-06-13 04:07:57 +0600

    time accurecy

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,7 +15,8 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $nextThreeMatches = Fixture::where('date', '>=', now()->addDay())//->addDay()
+
+        $nextThreeMatches = Fixture::where('date', '>=', now()->setTimezone('Asia/Dhaka')->addDay())
             ->where('actual_team1_goals', '=', null)
             ->with(['team1', 'team2'])
             ->orderBy('date', 'asc')
