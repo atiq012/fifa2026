2de097581050e75ff43be26e2f2713770bc6c866
atiq012 <shakilratiq@gmail.com>
2026-06-18 12:45:53 +0600

    all matches for 28

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,7 +17,7 @@ class DashboardController extends Controller
     {
 
         $nextThreeMatches = Fixture::where('date', '>=', now()->setTimezone('Asia/Dhaka')->startOfDay())
-            ->where('date', '<=', now()->setTimezone('Asia/Dhaka')->addDays(2))
+            // ->where('date', '<=', now()->setTimezone('Asia/Dhaka')->addDays(2))
             ->where('actual_team1_goals', '=', null)
             ->with(['team1', 'team2'])
             ->orderBy('date', 'asc')
